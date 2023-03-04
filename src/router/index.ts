85c96c55a00e7d28c09b00f9b57cc0d701335058
Nosa-Drexx <nosakhare.egharevba@physci.uniben.edu>
2023-03-04 07:30:03 +0100

    router updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import NoteView from '../views/NoteView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'notes',
+      component: NoteView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/starred',
+      name: 'starred',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/StarredNotesView.vue')
+    },
+    {
+      path: '/trash',
+      name: 'trash',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/TrashNotesView.vue')
     }
   ]
 })
---
+++ src/views/NoteView.vue
@@ -0,0 +1,22 @@
+<script>
+import AddNotes from '@/components/addNotes.vue'
+import NoteSection from '@/components/NoteSection.vue'
+</script>
+
+<template>
+  <section>
+    <header className="w-screen h-auto flex justify-between bg-gray-100 p-2 text-2xl">
+      <h1>Welcome to notes</h1>
+      <ul className="list-none flex gap-4">
+        <li>
+          <router-link to="/starred" className="underline"> Starred </router-link>
+        </li>
+        <li>
+          <router-link to="/trash" className="underline"> Trash </router-link>
+        </li>
+      </ul>
+    </header>
+    <AddNotes />
+    <NoteSection />
+  </section>
+</template>
